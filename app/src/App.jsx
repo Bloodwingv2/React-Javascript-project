904849e244f9f12decdb5615819ffc163e142b4a
Bloodwingv2 <bhandariumesh81@gmail.com>
2025-09-23 12:52:38 +0530

    Implementing new components to check their tags

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,10 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() { // Functional Arrow Component to export the componenent and testing react arch
+const Card = () => {
+  return (
+    <div>
+      <h2> Card Component</h2>
+    </div>
+  )
+}
+
+const App = () => { // Functional Arrow Component to export the componenent and testing react arch
 
   return (
-    <h2> Functional Arrow Component</h2>
+    <div>
+      <h2> Functional Arrow Component</h2>
+      <Card />
+      <Card />
+    </div>
   )
 }
 
