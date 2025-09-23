c5fdfc5da9456f0b0f64dee733492ea9449551fa
Bloodwingv2 <bhandariumesh81@gmail.com>
2025-09-23 12:48:04 +0530

    Created A demo Architecture to understand React fundamentals

---
+++ app/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() { // Functional Arrow Component to export the componenent and testing react arch
+
+  return (
+    <h2> Functional Arrow Component</h2>
+  )
+}
+
+export default App
