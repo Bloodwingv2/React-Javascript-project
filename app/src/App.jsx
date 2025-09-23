481d8039242c2f21b3f32e3bca8cd3aabd8d74df
Bloodwingv2 <bhandariumesh81@gmail.com>
2025-09-23 19:18:45 +0530

    Refactor Card component to include title prop and state management; update App component to render multiple Card instances

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,23 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const Card = () => {
-  return (
-    <div>
-      <h2> Card Component</h2>
+const Card = ({title}) => { // Functional Arrow Component to Test use states and changing variables!
+  const [count, setcount] = useState(0);
+  const [hasLiked, setHasLiked] = useState(false);
+  
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`);
+  }, [hasLiked]); // Effect hook to log console info using a dependency array
+
+  useEffect(() => {
+    console.log(`CARD RENDERED`)
+  },[]); // Useffect for logging
+  
+
+  return ( // conditional rendering tests
+    <div className = "card" onClick={() => setcount(count + 1)}>
+      <h2>{title}<br/>{count || null}</h2> 
+      <button onClick={() => setHasLiked(!hasLiked)}> 
+        {hasLiked ? '❤️': '🤍'} 
+      </button>
     </div>
   )
 }
 
 const App = () => { // Functional Arrow Component to export the componenent and testing react arch
-
   return (
-    <div>
-      <h2> Functional Arrow Component</h2>
-      <Card />
-      <Card />
+    <div className = "card-container">
+      <Card title ="Star Wars" />
+      <Card title = "Avengers" />
+      <Card title = "Conjuring" />
     </div>
   )
 }
