05122c9467cd4c30ca6429d9db31cbc7ca779ce6
Bloodwingv2 <bhandariumesh81@gmail.com>
2025-09-24 22:59:38 +0530

    Added More error handling scripts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 import Search from './components/search.jsx'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS ={
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
+
+
 
 const App = () => {
   const [searchTerm, setsearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+
+  const fetchMovies = async () => {
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if(!response.ok){
+        throw new Error('Failed to fetch Movies');
+      }
+
+      const data = await response.json();
+      if (data.response == 'False'){
+        setErrorMessage(data.error || 'Failed to fetch Movies');
+      }
+
+    } catch(error) {
+      console.error(`Error Fetching Movies: ${error}`);
+      setErrorMessage("Error Fetching Movies, Please Try Again Later!")
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+  
 
   return (
     <main>
@@ -18,6 +59,10 @@ const App = () => {
 
           <Search searchTerm={searchTerm} setsearchTerm={setsearchTerm}/>
           <h1>{searchTerm}</h1>
+           <section className='all-movies'>
+            <h2>All Movies</h2>
+            {errorMessage} && <p className='text-red-500'>{errorMessage}</p>
+          </section>
         </div>
     </main>
   )
